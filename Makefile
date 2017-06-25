d6ccd117c3cba385ec85c45e67ab833e8cb8bf6e
Victor Springer <victorspringer@gmail.com>
2017-06-24 23:11:03 -0300

    adds development environment

--- Makefile
+++ Makefile
@@ -23,4 +23,15 @@ start:
 	export FB_CLIENT_ID="" && \
 	export FB_CLIENT_SECRET="" && \
 	go get && go build -v -o bin/application && ./bin/application
+start-dev:
+	export ENV="development" && \
+	export DOMAIN="localhost:8080" && \
+	export PORT="8080" && \
+	export DB_USER="root" && \
+	export DB_PASS="" && \
+	export DB_ADDR="localhost" && \
+	export DB_PORT="3306" && \
+	export FB_CLIENT_ID="" && \
+	export FB_CLIENT_SECRET="" && \
+	go get && go build -v -o bin/application && ./bin/application
 
--- authenticating/middleware.go
+++ authenticating/middleware.go
@@ -3,56 +3,59 @@ package authenticating
 import (
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 )
 
 func (s *service) AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cookieTravellerID, err := r.Cookie("travellerID")
-		if err != nil {
-			log.Println(err)
-			http.Error(w, http.StatusText(401), 401)
-			return
-		}
+		if os.Getenv("ENV") == "production" {
+			cookieTravellerID, err := r.Cookie("travellerID")
+			if err != nil {
+				log.Println(err)
+				http.Error(w, http.StatusText(401), 401)
+				return
+			}
 
-		cookieSessionToken, err := r.Cookie("sessionToken")
-		if err != nil {
-			log.Println(err)
-			http.Error(w, http.StatusText(401), 401)
-			return
-		}
+			cookieSessionToken, err := r.Cookie("sessionToken")
+			if err != nil {
+				log.Println(err)
+				http.Error(w, http.StatusText(401), 401)
+				return
+			}
 
-		id, err := strconv.Atoi(cookieTravellerID.Value)
-		if err != nil {
-			log.Println(err)
-			http.Error(w, http.StatusText(401), 401)
-			return
-		}
+			id, err := strconv.Atoi(cookieTravellerID.Value)
+			if err != nil {
+				log.Println(err)
+				http.Error(w, http.StatusText(401), 401)
+				return
+			}
 
-		if err = s.ValidateSession(id, cookieSessionToken.Value); err != nil {
+			if err = s.ValidateSession(id, cookieSessionToken.Value); err != nil {
+				cookieTravellerID.Path = "/"
+				cookieTravellerID.MaxAge = -1
+				cookieSessionToken.Path = "/"
+				cookieSessionToken.MaxAge = -1
+
+				http.SetCookie(w, cookieTravellerID)
+				http.SetCookie(w, cookieSessionToken)
+
+				log.Println(err)
+				http.Error(w, http.StatusText(401), 401)
+				return
+			}
+
+			expires := time.Now().Add(30 * 24 * time.Hour)
+
 			cookieTravellerID.Path = "/"
-			cookieTravellerID.MaxAge = -1
+			cookieTravellerID.Expires = expires
 			cookieSessionToken.Path = "/"
-			cookieSessionToken.MaxAge = -1
+			cookieSessionToken.Expires = expires
 
 			http.SetCookie(w, cookieTravellerID)
 			http.SetCookie(w, cookieSessionToken)
-
-			log.Println(err)
-			http.Error(w, http.StatusText(401), 401)
-			return
 		}
-
-		expires := time.Now().Add(30 * 24 * time.Hour)
-
-		cookieTravellerID.Path = "/"
-		cookieTravellerID.Expires = expires
-		cookieSessionToken.Path = "/"
-		cookieSessionToken.Expires = expires
-
-		http.SetCookie(w, cookieTravellerID)
-		http.SetCookie(w, cookieSessionToken)
 
 		next(w, r)
 	})
