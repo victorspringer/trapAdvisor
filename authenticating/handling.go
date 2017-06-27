2280aef7c152de5835282b03dd478e74db45d757
Victor Springer <victorspringer@gmail.com>
2017-06-27 03:55:14 -0300

    adds redirect on fb rejection callback

--- authenticating/handling.go
+++ authenticating/handling.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"net/url"
@@ -62,7 +63,12 @@ func (s *service) HandleFacebookCallback(w http.ResponseWriter, r *http.Request)
 				<script>history.go(-2)</script>
 			</html>
 		`
-		w.Write([]byte(callback))
+		tmpl := template.New("callback")
+		var err error
+		if tmpl, err = tmpl.Parse(callback); err != nil {
+			log.Println(err)
+		}
+		tmpl.Execute(os.Stdout, callback)
 		return
 	}
 
@@ -188,7 +194,11 @@ func (s *service) HandleFacebookCallback(w http.ResponseWriter, r *http.Request)
 			<script>history.back()</script>
 		</html>
 	`
-	w.Write([]byte(callback))
+	tmpl := template.New("callback")
+	if tmpl, err = tmpl.Parse(callback); err != nil {
+		log.Println(err)
+	}
+	tmpl.Execute(os.Stdout, callback)
 }
 
 func (s *service) HandleFacebookLogout(w http.ResponseWriter, r *http.Request) {
